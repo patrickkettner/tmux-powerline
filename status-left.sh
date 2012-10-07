b7a1c1321b732491784641f7cf9b1e22a40b3fe2
Patrick Kettner <patrickkettner@gmail.com>
2012-10-07 01:53:11 -0400

    hide vcs - too slow

--- status-left.sh
+++ status-left.sh
@@ -53,7 +53,7 @@ vcs_branch+=(["script"]="${segments_path}/vcs_branch.sh")
 vcs_branch+=(["foreground"]="colour88")
 vcs_branch+=(["background"]="colour29")
 vcs_branch+=(["separator"]="${separator_right_bold}")
-register_segment "vcs_branch"
+#register_segment "vcs_branch"
 
 declare -A background_jobs
 background_jobs+=(["script"]="${segments_path}/jobs.sh")
